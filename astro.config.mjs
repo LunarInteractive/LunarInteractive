819efa57908257fc4fc8f443bd99deab9bb64807
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-05-27 22:29:34 +0700

    deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lunarinteractive.github.io",
-  base: "/LunarInteractive",
+  base: "/main",
   integrations: [tailwind()]
 });
