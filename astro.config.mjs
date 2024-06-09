c47b6622daca9d076337f902ec4046c9b417cc4a
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-06-10 02:02:35 +0700

    deployment test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lunarinteractive.github.io",
-  base: "/LunarInteractive/",
-  trailingSlash: "always",
+  trailingSlash: "ignore",
   output: "server",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
