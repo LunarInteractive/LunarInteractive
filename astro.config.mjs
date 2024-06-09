519212d1da273393604c0963c0e87c88a9843d9a
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-06-09 17:40:36 +0700

    footer and crew cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lunarinteractive.github.io",
   base: "/LunarInteractive",
   trailingSlash: "always",
-  
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
